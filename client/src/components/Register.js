18a5669aefb7eff28ae3cb4a7e516fa7912819b2
arthurbz <arthurbziero@gmail.com>
2021-06-23 22:39:35 -0300

    Creating login route

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,9 +1,74 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
-const Register = () => {
+const Register = ({ setAuth }) => {
+    const [inputs, setInputs] = useState({
+        name: "",
+        email: "",
+        password: ""
+    });
+
+    const { name, email, password } = inputs;
+
+    const onChange = event => {
+        setInputs({
+            ...inputs, [event.target.name]
+                : event.target.value
+        });
+    };
+
+    const onSubmitForm = async event => {
+        event.preventDefault();
+
+        try {
+            const body = { name, email, password }
+
+            const response = await fetch("http://localhost:5000/auth/register",
+                {
+                    method: "POST",
+                    headers: { "Content-type": "application/json" },
+                    body: JSON.stringify(body)
+                });
+
+            const parseResponse = await response.json();
+            
+            localStorage.setItem("token", parseResponse.token);
+
+            setAuth(true);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
     return (
         <Fragment>
-            <h1>Register</h1>
+            <h1 className="text-center my-5">Register</h1>
+            <form onSubmit={onSubmitForm}>
+                <input
+                    type="name"
+                    name="name"
+                    placeholder="Name"
+                    className="form-control my-3"
+                    value={name}
+                    onChange={event => onChange(event)}
+                />
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    className="form-control my-3"
+                    value={email}
+                    onChange={event => onChange(event)}
+                />
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    className="form-control my-3"
+                    value={password}
+                    onChange={event => onChange(event)}
+                />
+                <button className="btn btn-success btn-block">Submit</button>
+            </form>
         </Fragment>
     );
 }
